cfcaba3656a829537e3d6ed847c6c848aef227f9
Sarah Naas <sjnaas@gmail.com>
2024-09-05 14:50:10 -0700

    connect to pg

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require("express");
+require("dotenv").config();
+
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-require("dotenv").config();
+const pool = require("./db");
 
 const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/todos", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM todos");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
