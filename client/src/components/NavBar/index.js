0a4a31824565c59c1594681fea9f186527ceb51d
Sarah Naas <sjnaas@gmail.com>
2024-09-07 17:09:02 -0700

    title styles

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -6,11 +6,21 @@ export const StyledHeader = styled.header`
   height: 10vh;
   background-color: teal;
   min-height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledTitle = styled.h1`
+  color: aliceblue;
+  font-family: Optima, sans-serif;
+  margin: 0;
 `;
 
 function NavBar() {
   return (
     <StyledHeader>
+      <StyledTitle>OmList</StyledTitle>
       <IconButton icon="Hello" />
     </StyledHeader>
   );
