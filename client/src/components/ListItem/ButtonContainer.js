71508977eb07a8b812665e0cf44c61c3f5e9d994
Sarah Naas <sjnaas@gmail.com>
2024-10-17 16:22:19 +0200

    created button container component

---
+++ client/src/components/ListItem/ButtonContainer.js
@@ -0,0 +1,72 @@
+import React from "react";
+import styled from "styled-components";
+import { CheckmarkIcon, PlusIcon, TrashIcon } from "../Icons";
+
+const StyledButton = styled.button`
+  border: none;
+  background: transparent;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+
+function ButtonContainer({ id }) {
+  async function deleteTodos(id) {
+    try {
+      await axios
+        .request({
+          method: "DELETE",
+          url: `/api/todos/${id}`,
+        })
+        .then((res) => {
+          console.log(res);
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async function markTodoAsComplete(id) {
+    try {
+      await axios.put(`/api/todos/${id}`, { progress: 100 }).then((res) => {
+        console.log(res);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  return (
+    <StyledButtonContainer>
+      <StyledButton>
+        <CheckmarkIcon
+          width="40"
+          height="40"
+          stroke="darkseagreen"
+          onClick={() => markTodoAsComplete(id)}
+        />
+      </StyledButton>
+      <StyledButton>
+        <PlusIcon width="40" height="40" stroke="darkseagreen" />
+      </StyledButton>
+      <StyledButton>
+        <TrashIcon
+          width="20"
+          height="20"
+          stroke="red"
+          onClick={() => deleteTodos(id)}
+        />
+      </StyledButton>
+    </StyledButtonContainer>
+  );
+}
+
+export default ButtonContainer;
