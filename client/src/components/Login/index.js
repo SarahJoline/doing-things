bc803e30fb6545e5bba52a98ed2785c578ec1bac
Sarah Naas <sjnaas@gmail.com>
2024-10-27 11:34:17 +0100

    login component

---
+++ client/src/components/Login/index.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import React, { useState } from "react";
+import AuthHelperMethods from "../../auth";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const payload = {
+      email,
+      password,
+    };
+    try {
+      const { data } = await axios.post("/api/user/log-in", payload);
+
+      AuthHelperMethods.setToken(data.token);
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
